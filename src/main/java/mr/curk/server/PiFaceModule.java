d3c129c384e2e7572f568e5822c795c9f70b95d1
Mr.Curk <mr.curk@gmail.com>
2016-07-08 15:47:12 +0200

    add piface interface

--- src/main/java/mr/curk/server/PiFaceModule.java
+++ src/main/java/mr/curk/server/PiFaceModule.java
@@ -3,6 +3,7 @@ package mr.curk.server;
 
 import com.pi4j.device.piface.PiFace;
 import com.pi4j.device.piface.impl.PiFaceDevice;
+import mr.curk.util.CommandStatus;
 
 import java.io.IOException;
 
@@ -16,4 +17,68 @@ public class PiFaceModule {
     }
 
     //SET COMMAND FOR PIFACE MODULE
+    public void setCommand(CommandStatus command) {
+        switch (command) {
+            case OUTPUT_0_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_0_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_1_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_1_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_2_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_2_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_3_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_3_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_4_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_4_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_5_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_5_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_6_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_6_ON:
+                setOutputOn(0);
+                break;
+            case OUTPUT_7_OFF:
+                setOutputOff(0);
+                break;
+            case OUTPUT_7_ON:
+                setOutputOn(0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //SET OUTPUT OFF
+    private void setOutputOff(int pin) {
+        piFace.getOutputPin(pin).low();
+    }
+
+    //SET OUTPUT ON
+    private void setOutputOn(int pin) {
+        piFace.getOutputPin(pin).high();
+    }
 }
